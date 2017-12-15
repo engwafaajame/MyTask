caed6781f3784c8d16af2f34f05990f3b2f9e9d5
wafaa_200066@hotmail.com <wafaa29121988>
2017-12-15 19:11:14 +0200

    yes

---
+++ app/src/main/java/com/example/eng_wafaa/mytask/Calender.java
@@ -0,0 +1,22 @@
+package com.example.eng_wafaa.mytask;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+public class Calender extends AppCompatActivity {
+
+    @TargetApi(Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+        DatePicker time = findViewById(R.id.calendarView);
+
+    }
+}
